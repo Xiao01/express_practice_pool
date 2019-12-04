ca301daccd6328f1803b541707d36a733777317c
Xiao01 <401607178@qq.com>
2019-12-04 16:11:57 +0800

    todolist项目 express+mongodb实现

---
+++ todolist_demo/controllers/todoController.js
@@ -0,0 +1,36 @@
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://@localhost:27017/xiao');
+
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+module.exports = function(app) {
+  app.get('/todo', function(req, res) {
+    Todo.find({}, function(err, data) {
+      if (err) throw err;
+      res.render('todo', { todos: data });
+    });
+  });
+
+  app.post('/todo', urlencodedParser, function(req, res) {
+    var itemOne = Todo(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+
+  app.delete('/todo/:item', function(req, res) {  
+    Todo.find({item: req.params.item.replace(/-/g, " ")}).remove(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  });
+}
