b0b4c4d01414431f61e1bc06bd8a3c493998e07c
Xiao01 <401607178@qq.com>
2019-12-04 22:41:14 +0800

    project项目导入bootstarp定义样式

--- project/routes/index.js
+++ project/routes/index.js
@@ -7,13 +7,13 @@ router.get('/', function(req, res, next) {
   model.connect(function(db){
 	  db.collection(USERS_COLLECTION).find().toArray(function(arr,docs){
 		  console.log("用户列表："+ JSON.stringify(docs));
-		  res.render('index', { title: 'Express',data: JSON.stringify(docs)});
+		  res.render('index', { title: '首页',data: JSON.stringify(docs)});
 	  })
   })
 });
 
-router.get('/bootstart', function(req, res, next) {
-	res.render('bootstart');
+router.get('/bootstarp', function(req, res, next) {
+	res.render('bootstarp');
 });
 
 
--- project/routes/users.js
+++ project/routes/users.js
@@ -2,61 +2,78 @@ var express = require('express');
 var router = express.Router();
 var model = require('../model');
 const USERS_COLLECTION = "users"
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-	res.send('respond with a resource');
+/* 访问用户列表页 */
+router.get('/', function(req, res, next) {
+	model.connect(function(db){
+		db.collection(USERS_COLLECTION).find().toArray(function(arr,docs){
+			console.log("用户列表："+ JSON.stringify(docs));
+			res.render('users', { title: '用户列表页',data: docs});
+		})
+	})
 });
+
+//访问登录页面
 router.get('/login', function (req, res, next) {
-	res.render("login");
+	res.render("login",{title:"访问登录页面"});
 });
 //访问注册页面
 router.get('/regist', function (req, res, next) {
-	res.render('regist');
+	res.render('regist',{title:"访问注册页面"});
 
 });
-//注册账号
+//提交注册账号
 router.post('/regist', function (req, res, next) {
 	console.log(req.body.username)
 	console.log(req.body.password)
 	console.log(req.body.password2)
+	var data={
+		username:req.body.username,
+		password:req.body.password,
+		password2:req.body.password2
+	}
 	if (req.body.username != null && req.body.password != null && req.body.password == req.body.password2) {
 		model.connect(function (db) {
-			db.collection(USERS_COLLECTION).insertOne(req.body, function (err, doc) {
+			db.collection(USERS_COLLECTION).insertOne(data, function (err, doc) {
 				if (err) {
 					console.log(res, err.message, "Failed to create new user.");
+					res.send("<br>  注册失败,<a href='/users/regist'>重新注册</a>");
 				} else {
-					res.redirect('/users/login');
+					res.redirect('login');
 				}
 			});
 		})
 	} else {
 		console.log(" req.body.username !=null && req.body.password !=null && req.body.password==req.body.password2");
-		res.status(201).json(req.body);
+		res.send("<br> 注册失败, <a href='/users/regist'>重新注册</a>"+" <br>用户名："+ req.body.username  +" <br>密码："+req.body.password +" ,<br>确认密码："+req.body.password2 );
 	}
 });
 
 
-
+//提交登录账号
 router.post('/login', function (req, res, next) {
 	console.log(req.body.username)
 	console.log(req.body.password)
+	var data={
+		username:req.body.username,
+		password:req.body.password
+	}
 	if (req.body.username != null && req.body.password != null) {
 		model.connect(function (db) {
-			console.log(" req.body")
-			console.log(req.body)
-			db.collection(USERS_COLLECTION).find(req.body, function (err, doc) {
-				console.log(" doc")
-				console.log(doc)
+			db.collection(USERS_COLLECTION).find(data).toArray( function (err, docs) {			
 				if (err) {
-					console.log(res, err.message, "Failed to find user.");
+					res.send("<br>  登录失败,<a href='/users/login'>重新登录</a>");
 				} else {
-					res.redirect('/');
+					if( docs.length>0){
+						res.redirect('/');
+					}else{
+						res.send("<br> 账号密码错误，<a href='/users/login'>重新登录</a>");
+					}
+					
 				}
 			});
 		})
 	} else {
-		console.log(" req.body.username !=null && req.body.password !=null ");
-		res.status(201).json(req.body);
+		res.send("<br> 登录失败, <a href='/users/login'>重新登录</a>"+" <br>用户名："+ req.body.username  +" <br>密码："+req.body.password );
 	}
 });
 
