0b072ed9a3b6da7079f387e2f757a4675ff0312a
Xiao01 <401607178@qq.com>
2019-12-06 20:17:01 +0800

    优化登录拦截，登出等模块代码

--- project/app.js
+++ project/app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,30 +27,59 @@ app.use(session({
   secret: 'session',
   resave: true,
   saveUninitialized:true,
-  cookie: { maxAge:1000*60*5 }
+  cookie: { maxAge:1000*60*50 }
 }))
+// //登录拦截
+// app.get('*',function(req,res,next){
+// 	var path = req.path;
+// 	var username =req.session.username;
+// 	console.log("登录拦截",username  ," ,path:",path);
+// 	if( path =="/" || path =="/write"){
+// 		if(!username){
+// 			console.log("跳转到登录页面")
+// 			res.redirect("/users/login");
+// 		}
+// 	}
+// 	next()
+// })
 //登录拦截
-app.get('*',function(req,res,next){
-	var path = req.path;
-	var username =req.session.username;
-	console.log("登录拦截",username  ," ,path:",path);
-	if(path =="/users" || path =="/" || path =="/write"){
-		if(!username){
-			console.log("跳转到登录页面")
-			res.redirect("/users/login");
-		}
-	}
-	next()
-})
+
+
+
+
+
+
+//登录拦截
+app.use(function (req, res, next) {
+  console.log("登录拦截")
+  if (req.session.username) {  // 判断用户是否登录
+    next();
+  } else {
+    console.log(req.url )
+    // 解析用户请求的路径
+    var arr = req.url.split('/');
+    // 去除 GET 请求路径上携带的参数
+    for (var i = 0, length = arr.length; i < length; i++) {
+      arr[i] = arr[i].split('?')[0];
+    }
+    // 判断请求路径是否为根、登录、注册、登出，如果是不做拦截
+    if (arr.length > 1 && arr[1] == '') {
+      next();
+    } else if (arr.length > 2 && arr[1] == 'users' && (arr[2] == 'regist' || arr[2] == 'login' || arr[2] == 'logout')) {
+      next();
+    } else {  // 登录拦截
+      req.session.originalUrl = req.originalUrl ? req.originalUrl : null;  // 记录用户原始请求路径
+     
+      res.redirect('/users/login');  // 将用户重定向到登录页面
+    }
+  }
+});
+
+
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,5 +96,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- project/routes/users.js
+++ project/routes/users.js
@@ -5,57 +5,79 @@ var moment = require('moment');
 
 const USERS_COLLECTION = "users";
 /* 访问用户列表页 */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	var username = req.session.username || "";
-	model.connect(function(db){
-		db.collection(USERS_COLLECTION).find().toArray(function(arr,docs){
-			console.log("用户列表："+ JSON.stringify(docs));
-			res.render('users', { title: '用户列表页面',data: docs,username: username});
+	model.connect(function (db) {
+		db.collection(USERS_COLLECTION).find().toArray(function (arr, docs) {
+			console.log("用户列表：" + JSON.stringify(docs));
+			res.render('users', { title: '用户列表页面', data: docs, username: username });
 		})
 	})
 });
 
 //访问登录页面
 router.get('/login', function (req, res, next) {
-	res.render("login",{title:"登录页面"});
+	res.render("login", { title: "登录页面" });
 });
 //访问注册页面
 router.get('/regist', function (req, res, next) {
-	res.render('regist',{title:"注册页面"});
+	res.render('regist', { title: "注册页面" });
 });
 
 
 //提交用户登出
 router.get('/logout', function (req, res, next) {
-	req.session.username=null;
-	req.session.password=null;
-	res.redirect("/users/login");
+	var username = req.session.username || "" ;
+	if( username){
+		res.render("logout",{title:req.session.username+"，您是否确认退出?"});
+	}else{
+		res.send("会话过期，无需退出, <a href='/'>返回主页</a>")
+	}
+});
+
+router.get('/logout_conform', function (req, res, next) {
+	req.session.username ="" ;
+	req.session.password ="" ;
+	
+		res.send("退出成功, <a href='/'>返回主页</a>");
+	
 });
 
 //提交注册账号
 router.post('/regist', function (req, res, next) {
+	console.log("//提交注册账号")
 	console.log(req.body.username)
 	console.log(req.body.password)
 	console.log(req.body.password2)
-	var data={
-		username:req.body.username,
-		password:req.body.password,
-		password2:req.body.password2
+	var is_new = false;
+	moment.locale('zh-cn');
+	var _today = moment().format('YYYY-MM-DD HH:mm:ss');
+	var whereData = { username: req.body.username }
+	var data = {
+		username: req.body.username,
+		password: req.body.password,
+		phone: req.body.phone,
+		last_datetime: _today
 	}
-	if (req.body.username != null && req.body.password != null && req.body.password == req.body.password2) {
+	if (req.body.username != null && req.body.password != null && req.body.password == req.body.password2
+		&& req.body.phone != null) {
+
 		model.connect(function (db) {
 			db.collection(USERS_COLLECTION).insertOne(data, function (err, doc) {
 				if (err) {
-					console.log(res, err.message, "Failed to create new user.");
+					console.log(res,"---------", err.message);
 					res.send("<br>  注册失败,<a href='/users/regist'>重新注册</a>");
 				} else {
-					res.redirect('login');
+					//登录成功，进行session存储
+					req.session.username = data.username;
+					req.session.password = data.password;
+					res.redirect('/');
 				}
 			});
 		})
 	} else {
 		console.log(" req.body.username !=null && req.body.password !=null && req.body.password==req.body.password2");
-		res.send("<br> 注册失败, <a href='/users/regist'>重新注册</a>"+" <br>用户名："+ req.body.username  +" <br>密码："+req.body.password +" ,<br>确认密码："+req.body.password2 );
+		res.send("<br> 注册失败, <a href='/users/regist'>重新注册</a>" + " <br>用户名：" + req.body.username + " <br>密码：" + req.body.password + " ,<br>确认密码：" + req.body.password2);
 	}
 });
 
@@ -64,30 +86,30 @@ router.post('/regist', function (req, res, next) {
 router.post('/login', function (req, res, next) {
 	console.log(req.body.username)
 	console.log(req.body.password)
-	var data={
-		username:req.body.username,
-		password:req.body.password
+	var data = {
+		username: req.body.username,
+		password: req.body.password
 	}
 	if (req.body.username != null && req.body.password != null) {
 		model.connect(function (db) {
-			db.collection(USERS_COLLECTION).find(data).toArray( function (err, docs) {			
+			db.collection(USERS_COLLECTION).find(data).toArray(function (err, docs) {
 				if (err) {
 					res.send("<br>  登录失败,<a href='/users/login'>重新登录</a>");
 				} else {
-					if( docs.length>0){
+					if (docs.length > 0) {
 						//登录成功，进行session存储
-						req.session.username=data.username;
-						req.session.password=data.password;
+						req.session.username = data.username;
+						req.session.password = data.password;
 						res.redirect('/');
-					}else{
+					} else {
 						res.send("<br> 账号密码错误，<a href='/users/login'>重新登录</a>");
 					}
-					
+
 				}
 			});
 		})
 	} else {
-		res.send("<br> 登录失败, <a href='/users/login'>重新登录</a>"+" <br>用户名："+ req.body.username  +" <br>密码："+req.body.password );
+		res.send("<br> 登录失败, <a href='/users/login'>重新登录</a>" + " <br>用户名：" + req.body.username + " <br>密码：" + req.body.password);
 	}
 });
 
@@ -97,7 +119,26 @@ router.post('/login', function (req, res, next) {
 
 router.get('/userinfo', function (req, res, next) {
 	var username = req.session.username || "";
-	res.render('userinfo',{title:"修改用户密码",username: username});
+	var data = { username: username }
+	if (username != null) {
+		model.connect(function (db) {
+			db.collection(USERS_COLLECTION).find(data).toArray(function (err, docs) {
+				if (err) {
+					res.send("<br>获取用户信息失败,<a href='/users/userinfo'>重新获取用户信息</a>");
+				} else {
+					if (docs.length > 0) {
+						res.render('userinfo', { title: "修改用户信息", username: docs[0].username, phone: docs[0].phone });
+					} else {
+						res.send("<br>获取用户信息失败,<a href='/users/userinfo'>重新获取用户信息</a>");
+					}
+
+				}
+			});
+		})
+	} else {
+		res.send("<br> 登录失败, <a href='/users/login'>重新登录</a>" + " <br>用户名：" + req.body.username + " <br>密码：" + req.body.password);
+	}
+
 });
 
 
@@ -110,24 +151,26 @@ router.post('/userinfo_update', function (req, res, next) {
 	var _today = moment().format('YYYY-MM-DD HH:mm:ss');
 	var data = {
 		password: req.body.password,
+		phone: req.body.phone,
 		last_datetime: _today
 	}
 	var updateDat = { $set: data };
-	if(req.body.password !=null && req.body.password2 !=null && req.body.password == req.body.password2){
+	if (req.body.password != null && req.body.password2 != null && req.body.password == req.body.password2) {
 		model.connect(function (db) {
 			db.collection(USERS_COLLECTION).updateOne(whereData, updateDat, function (err, result) {
 				if (err) {
 					console.log('Error:' + err);
 				} else {
 					console.log(result);
-					req.session.username ="";
-					res.redirect('login');
+					
+					req.session.password =req.body.password
+					res.render('index', { title: "密码修改成功，返回主页", username: username });
 				}
 			});
 		})
+	} else {
+		res.render('userinfo', { title: "用户详情页面", username: username });
 	}
-	
-	res.render('userinfo',{title:"用户详情页面",username: username});
 });
 
 
